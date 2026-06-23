508456532cdd21fffa1c770888571cd665effdf5
ALEJANDRO <alejandroramirez7981@gmail.com>
2026-06-23 16:51:15 -0600

    admin por correo

--- src/libros/libros.controller.ts
+++ src/libros/libros.controller.ts
@@ -9,14 +9,16 @@ import {
   ParseIntPipe, 
   UseInterceptors, 
   UploadedFile,
-  UseGuards
+  UseGuards,
+  Res
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { extname } from 'path';
 import { LibrosService } from './libros.service';
 import { Libro } from './libro.entity';
-import { JwtAuthGuard } from '../auth/jwt-auth.guard'; // Importamos el Guard de seguridad
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { Response } from 'express';
 
 @Controller('libros')
 export class LibrosController {
@@ -27,13 +29,17 @@ export class LibrosController {
   // ==========================================
 
   @Get()
-  obtenerTodos() {
-    return this.librosService.obtenerTodos();
+  async obtenerTodos(@Res() res: Response) {
+    res.set('Cache-Control', 'no-store');
+    const libros = await this.librosService.obtenerTodos();
+    return res.json(libros);
   }
 
   @Get(':id')
-  obtenerPorId(@Param('id', ParseIntPipe) id: number) {
-    return this.librosService.obtenerPorId(id);
+  async obtenerPorId(@Param('id', ParseIntPipe) id: number, @Res() res: Response) {
+    res.set('Cache-Control', 'no-store');
+    const libro = await this.librosService.obtenerPorId(id);
+    return res.json(libro);
   }
 
   // ==========================================
@@ -58,21 +64,18 @@ export class LibrosController {
     return this.librosService.eliminar(id);
   }
 
-  // Ruta PROTEGIDA para manejar archivos (Imágenes)
   @UseGuards(JwtAuthGuard)
   @Post('imagen/:id')
   @UseInterceptors(FileInterceptor('portada', {
     storage: diskStorage({
-      destination: './uploads', // La carpeta donde caerán las fotos
+      destination: './uploads',
       filename: (req, file, cb) => {
-        // Genera un nombre único para que no se sobreescriban fotos con el mismo nombre
         const nombreUnico = Date.now() + '-' + Math.round(Math.random() * 1e9);
         cb(null, nombreUnico + extname(file.originalname)); 
       }
     })
   }))
   subirImagen(@Param('id', ParseIntPipe) id: number, @UploadedFile() file: Express.Multer.File) {
-    // Guarda el nombre del archivo en la base de datos
     return this.librosService.actualizar(id, { imagenUrl: file.filename });
   }
 }
